4c261e9fa91a149f4a3b0847957646e4b4a6eb28
jalu30122003 <pratomojalu@gmail.com>
2025-05-05 18:46:19 +0700

    model.py

--- model.py
+++ model.py
@@ -3,10 +3,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-# Load data
+
 data = pd.read_csv('crop_recommendation.csv')
 
-# Encode labels
+
 tanaman = ['rice', 'maize', 'chickpea', 'kidneybeans', 'pigeonpeas',
            'mothbeans', 'mungbean', 'blackgram', 'lentil', 'pomegranate',
            'banana', 'mango', 'grapes', 'watermelon', 'muskmelon', 'apple',
@@ -15,7 +15,7 @@ for x in range(len(tanaman)):
     ganti = {tanaman[x]: x}
     data['label'] = data['label'].replace(ganti)
 
-# Split data
+
 Train_set, Val_set = train_test_split(data, test_size=0.2, random_state=40)
 Test_set, Val_set = train_test_split(Val_set, test_size=0.5, random_state=40)
 
@@ -23,7 +23,7 @@ X_train, y_train = Train_set.drop(columns=['label']), Train_set['label']
 X_val, y_val = Val_set.drop(columns=['label']), Val_set['label']
 X_test, y_test = Test_set.drop(columns=['label']), Test_set['label']
 
-# Train Random Forest model
+
 model_rf = RandomForestClassifier(n_estimators=100, random_state=42)
 model_rf.fit(X_train, y_train)
 
